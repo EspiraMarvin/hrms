cc700887d7db195a0c0a9ab3561b4acd8744b3d8
EspiraMarvin <espiramarvin@gmail.com>
2020-04-29 15:23:24 +0300

    welcome page remove forgot password

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -102,23 +102,29 @@
                                     <div class="form-check">
                                         <input style="color: green !important" class="form-check-input" type="checkbox"
                                                name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>&nbsp;&nbsp; {{ __('Remember Me') }}
+
                                     </div>
+                                </div>
+                                <div style="float: right; margin-right: 20px">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Login') }}
+                                    </button>
                                 </div>
                             </div>
 
-                            <div class="form-group row mb-0">
+                            {{--<div class="form-group row mb-0">
                                 <div class=" offset-md-4">
                                     <button type="submit" class="btn btn-primary">
                                         {{ __('Login') }}
                                     </button>
 
-                                    @if (Route::has('password.request'))
+                                  --}}{{--  @if (Route::has('password.request'))
                                         <a class="btn btn-link" href="{{ route('password.request') }}">
                                             {{ __('Forgot Your Password?') }}
                                         </a>
-                                    @endif
+                                    @endif--}}{{--
                                 </div>
-                            </div>
+                            </div>--}}
                         </form>
                     </div>
                 </div>

70b3f7c502b28d3afe11af7f3c343e014d3a13f5
EspiraMarvin <espiramarvin@gmail.com>
2020-06-18 20:57:34 +0300

    hrms commit

---
+++ resources/views/Emails/leaveApprove.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Hello from ICT Authority
+
+I hope you're doing great, here is your leave application feedback!
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
